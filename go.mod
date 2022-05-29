a71f64b758f191f367e69ba45c6b85794fd8c886
ochom <ochomrichard752@gmail.com>
2022-05-29 13:46:43 +0300

    add event model

--- go.mod
+++ go.mod
@@ -2,9 +2,17 @@ module github.com/ochom/event-utils
 
 go 1.18
 
-require github.com/go-redis/redis/v8 v8.11.5
+require (
+	github.com/go-redis/redis/v8 v8.11.5
+	gorm.io/datatypes v1.0.6
+)
 
 require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
+	gorm.io/driver/mysql v1.3.2 // indirect
+	gorm.io/gorm v1.23.2 // indirect
 )
--- models/models.go
+++ models/models.go
@@ -1,8 +1,77 @@
 package models
 
 import (
+	"encoding/json"
+	"fmt"
 	"time"
+
+	"gorm.io/datatypes"
 )
+
+// Event ...
+type Event struct {
+	ID             string         `json:"id,omitempty"`
+	OrganizationID string         `json:"organizationID,omitempty"`
+	Name           string         `json:"name,omitempty"`
+	Image          string         `json:"image,omitempty"`
+	Category       string         `json:"category,omitempty"`
+	Description    string         `json:"description,omitempty"`
+	Venue          string         `json:"venue,omitempty"`
+	StartTime      time.Time      `json:"startTime,omitempty"`
+	EndTime        time.Time      `json:"endTime,omitempty"`
+	Facebook       string         `json:"facebook,omitempty"`
+	Twitter        string         `json:"twitter,omitempty"`
+	HashTags       string         `json:"hashTags,omitempty"`
+	TnC            string         `json:"tnc,omitempty"`
+	Tickets        datatypes.JSON `json:"tickets,omitempty"`
+	CreatedAt      time.Time      `json:"createdAt,omitempty"`
+	UpdatedAt      time.Time      `json:"updatedAt,omitempty"`
+}
+
+// SetTickets ...
+func (e *Event) SetTickets(data []EventTicket) error {
+	b, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	e.Tickets = b
+
+	return nil
+}
+
+// GetTickets ...
+func (e *Event) GetTickets() ([]*EventTicket, error) {
+	data := []*EventTicket{}
+	if err := json.Unmarshal(e.Tickets, &data); err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+// GetTicket gets one ticket by name
+func (e *Event) GetTicket(name string) (*EventTicket, error) {
+	tickets, err := e.GetTickets()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, v := range tickets {
+		if v.Name == name {
+			return v, nil
+		}
+	}
+
+	return nil, fmt.Errorf("ticket `%s` not found", name)
+}
+
+// EventTicket ...
+type EventTicket struct {
+	Name     string    `json:"name,omitempty"`
+	Cost     int       `json:"cost,omitempty"`
+	Slots    int       `json:"slots,omitempty"`
+	Deadline time.Time `json:"deadline,omitempty"`
+}
 
 // Payment ...
 type Payment struct {
